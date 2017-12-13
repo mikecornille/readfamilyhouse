e8e3d9a949bf948ea2b979d8b5baee49ac651228
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2017-12-13 11:08:52 -0600

    changed from collectives to html forms

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::group(['middleware' => 'auth'],function() 
+{	
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('reservation', 'ReservationController');
+
+});

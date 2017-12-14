daea82d01419a914729d62fb066e43d4e36b2386
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2017-12-14 14:30:39 -0600

    Added email functionality

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/info';
 
     /**
      * Create a new controller instance.

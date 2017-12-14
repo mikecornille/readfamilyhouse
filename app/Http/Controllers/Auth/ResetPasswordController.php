31de0d1c5e57019ef50eddf373dbe5a609e32b85
Michael Cornille <michaelcornille@Michaels-MacBook-Pro.local>
2017-12-14 13:47:20 -0600

    Added email functionality

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/info';
 
     /**
      * Create a new controller instance.
